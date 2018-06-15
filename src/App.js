7a35326c166282da33577445a4ef2ad8188902db
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-06-15 19:37:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import runner from './Running.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={runner} className="App-logo" alt="logo" />
+          <h1 className="App-title">Running Log Home</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
